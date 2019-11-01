10b0be84722dccaf9e05bcc5084dcc8b8d251241
Tobias Locker <tobias@tobiaslocker.de>
2019-11-01 08:11:37 +0000

    Add some pip

--- post-install.sh
+++ post-install.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 sudo apt update
+
 sudo apt install vim
 sudo apt install git
 sudo apt install curl
 sudo apt install build-essential cmake python3-dev
 sudo apt install python3-pip
-
-# YouCompleteMe
-mkdir -p ~/Workspace/github.com/ycm-core
-cd ~/Workspace/github.com/ycm-core
-git clone https://github.com/ycm-core/YouCompleteMe.git
-cd ~/.vim/bundle/YouCompleteMe
-# python3 install.py --clang-completer
-
-# Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # My rc files
 mkdir -p ~/Workspace/github.com/tobiaslocker
@@ -22,6 +13,18 @@ cd ~/Workspace/github.com/tobiaslocker
 git clone https://github.com/tobiaslocker/dotfiles.git
 ln -s ~/Workspace/github.com/tobiaslocker/dotfiles/.vimrc ~/.vimrc
 ln -s ~/Workspace/github.com/tobiaslocker/dotfiles/.global_extra_conf.py ~/.global_extra_conf.py
+
+# Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# YouCompleteMe
+mkdir -p ~/Workspace/github.com/ycm-core
+cd ~/Workspace/github.com/ycm-core
+git clone https://github.com/ycm-core/YouCompleteMe.git
+cd ~/.vim/bundle/YouCompleteMe
+vim +PluginInstall +qall
+python3 install.py --clang-completer
+
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim

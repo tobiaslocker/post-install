f052353882ed24dbcc93e45e89c7bf5481f3cc3b
Tobias Locker <tobias@tobiaslocker.de>
2019-11-06 19:19:32 +0000

    Install go

--- post-install.sh
+++ post-install.sh
@@ -13,24 +13,36 @@ git config --global user.name "Tobias Locker"
 git config --global core.editor "vim"
 
 # My rc files
-mkdir -p ~/Workspace/github.com/tobiaslocker
-cd ~/Workspace/github.com/tobiaslocker
+mkdir -p ~/Workspace/src/github.com/tobiaslocker
+cd ~/Workspace/src/github.com/tobiaslocker
 git clone https://github.com/tobiaslocker/dotfiles.git
-ln -s ~/Workspace/github.com/tobiaslocker/dotfiles/.vimrc ~/.vimrc
-ln -s ~/Workspace/github.com/tobiaslocker/dotfiles/.global_extra_conf.py ~/.global_extra_conf.py
+ln -s ~/Workspace/src/github.com/tobiaslocker/dotfiles/.vimrc ~/.vimrc
+ln -s ~/Workspace/src/github.com/tobiaslocker/dotfiles/.global_extra_conf.py ~/.global_extra_conf.py
+
+# Pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-# YouCompleteMe
-cd ~/.vim/bundle/YouCompleteMe
-python3 install.py --clang-completer
-
-
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
 
 python3 -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+
+GOVERSION=1.13.4
+OS=linux
+ARCH=amd64
+
+cd ~/Downloads
+wget https://dl.google.com/go/go${GOVERSION}.${OS}-${ARCH}.tar.gz
+
+cd ~/Downloads
+sudo tar -C /usr/local -xzf go${GOVERSION}.${OS}-${ARCH}.tar.gz
+
+
+
+# YouCompleteMe
+#cd ~/.vim/bundle/YouCompleteMe
+#python3 install.py --clang-completer
 

4e2eeb4960c263194817db0bd6537db7b1025e1e
Tobias Locker <tobias@tobiaslocker.de>
2019-11-07 18:16:16 +0000

    Add virtualenv

--- post-install.sh
+++ post-install.sh
@@ -1,57 +1,54 @@
 #!/bin/bash
-sudo apt update
+#sudo apt update
 
-sudo apt install vim
-sudo apt install git
-sudo apt install curl
-sudo apt install build-essential cmake python3-dev
-sudo apt install python3-pip
-sudo apt install vifm
-
-
-git config --global user.email "tobias@tobiaslocker.de"
-git config --global user.name "Tobias Locker"
-git config --global core.editor "vim"
-
-# My rc files
-mkdir -p ~/Workspace/src/github.com/tobiaslocker
-cd ~/Workspace/src/github.com/tobiaslocker
-git clone https://github.com/tobiaslocker/dotfiles.git
-ln -s ~/Workspace/src/github.com/tobiaslocker/dotfiles/.vimrc ~/.vimrc
-ln -s ~/Workspace/src/github.com/tobiaslocker/dotfiles/.global_extra_conf.py ~/.global_extra_conf.py
-ln -s ~/Workspace/src/github.com/tobiaslocker/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/Workspace/src/github.com/tobiaslocker/dotfiles/.bashrc ~/.bashrc
-
-# Pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-# Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+#sudo apt install vim
+#sudo apt install git
+#sudo apt install curl
+#sudo apt install build-essential cmake python3-dev
+#sudo apt install python3-pip
+#sudo apt install vifm
 
 
-python3 -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+#git config --global user.email "tobias@tobiaslocker.de"
+#git config --global user.name "Tobias Locker"
+#git config --global core.editor "vim"
 
-GOVERSION=1.13.4
-OS=linux
-ARCH=amd64
+#mkdir -p ~/Workspace/src/github.com/tobiaslocker
+#cd ~/Workspace/src/github.com/tobiaslocker
+#git clone https://github.com/tobiaslocker/dotfiles.git
+#ln -s ~/Workspace/src/github.com/tobiaslocker/dotfiles/.vimrc ~/.vimrc
+#ln -s ~/Workspace/src/github.com/tobiaslocker/dotfiles/.global_extra_conf.py ~/.global_extra_conf.py
+#ln -s ~/Workspace/src/github.com/tobiaslocker/dotfiles/.bash_profile ~/.bash_profile
+#ln -s ~/Workspace/src/github.com/tobiaslocker/dotfiles/.bashrc ~/.bashrc
 
-cd ~/Downloads
-wget https://dl.google.com/go/go${GOVERSION}.${OS}-${ARCH}.tar.gz
+#mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+#curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-cd ~/Downloads
-sudo tar -C /usr/local -xzf go${GOVERSION}.${OS}-${ARCH}.tar.gz
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#vim +PluginInstall +qall
 
-# Needed to install dependencies
-# of vim-go plugin 
-vim +GoInstallBinaries +qall
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#python3 -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
 
-# Install sources
-rustup component add rust-src
+#GOVERSION=1.13.4
+#OS=linux
+#ARCH=amd64
 
-# YouCompleteMe
-cd ~/.vim/bundle/YouCompleteMe
-python3 install.py --clang-completer --go-completer --rust-completer
+#cd ~/Downloads
+#wget https://dl.google.com/go/go${GOVERSION}.${OS}-${ARCH}.tar.gz
+
+#cd ~/Downloads
+#sudo tar -C /usr/local -xzf go${GOVERSION}.${OS}-${ARCH}.tar.gz
+
+#vim +GoInstallBinaries +qall
+
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+#rustup component add rust-src
+
+#cd ~/.vim/bundle/YouCompleteMe
+#python3 install.py --clang-completer --go-completer --rust-completer
+
+#python3 -m pip install --upgrade pip
+#python3 -m pip install --user tensorflow keras
+python3 -m pip install --user virtualenv
